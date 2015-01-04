f538afe7c55d61d2b53573aa0d8a5696768554f8
liaogang <ynwlgh@icloud.com>
2015-01-04 14:01:11 +0600

    Initial Commit

---
+++ Last.fm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Last.fm
+//
+//  Created by liaogang on 15/1/4.
+//  Copyright (c) 2015年 liaogang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
